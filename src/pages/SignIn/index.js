22d2f4e6ad0ad913221bd88fa6efe886507d6a7e
douglasgomes98 <douglasgomes.rr@gmail.com>
2019-10-31 19:17:51 -0400

    add styles singin and signup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
+import PropsTypes from 'prop-types';
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
+import Logo from '~/assets/logo.png';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SingLinkText,
+} from './styles';
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
   return (
     <Background>
-      <Input icon="call" placeholder="Digite seu nome" />
-      <Button>Entrar</Button>
+      <Container>
+        <Image source={Logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite o seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            blurOnSubmit={false}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SingLinkText>Criar conta gratuita</SingLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
+
+SignIn.propTypes = {
+  navigation: PropsTypes.shape({
+    navigate: PropsTypes.func.isRequired,
+  }).isRequired,
+};
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
+import PropsTypes from 'prop-types';
 import Background from '~/components/Background';
-// import { Container } from './styles';
+import Logo from '~/assets/logo.png';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SingLinkText,
+} from './styles';
 
-export default function SignUp() {
-  return <Background />;
+export default function SignUp({ navigation }) {
+  const passwordRef = useRef();
+  const emailRef = useRef();
+
+  function handleSubmit() {}
+
+  return (
+    <Background>
+      <Container>
+        <Image source={Logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+            blurOnSubmit={false}
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite o seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            blurOnSubmit={false}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SingLinkText>Já tenho conta</SingLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
 }
+
+SignUp.propTypes = {
+  navigation: PropsTypes.shape({
+    navigate: PropsTypes.func.isRequired,
+  }).isRequired,
+};
